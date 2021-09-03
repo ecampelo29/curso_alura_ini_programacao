c6a6b7b72b75b72342b1ae671946938e33085bd3
Emerson Campelo <63076222+ecampelo29@users.noreply.github.com>
2021-09-03 08:45:54 -0300

    Add files via upload

---
+++ projeto_inicial/scripts/listaDeTarefas.js
@@ -0,0 +1,26 @@
+
+/* encontra o elemento botão para interação */
+const novaTarefa = document.querySelector('[data-form-button]')
+
+/* função  para criar tarefa */
+const criarTarefa = (evento)=> {
+    /* previni que o formulário faça o encaminhamento dos dados */
+    evento.preventDefault()
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
+    
+    /**seleciona a lista e aplica o conteúdo novo */
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+
+    const conteudo = `<p class="content"> ${valor} </p>`
+    tarefa.innerHTML = conteudo
+
+    /**insere um novo item de lista */
+    lista.appendChild(tarefa)
+    input.value = " "
+}
+
+/** verifica se houve click no botão */
+novaTarefa.addEventListener('click', criarTarefa)
